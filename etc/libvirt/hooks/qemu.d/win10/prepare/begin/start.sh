8967212c3c1c0c7d80493fd4f7c9ffe0b20cd52f
Quentin Franchi <dev.quentinfranchi@noreply.localhost>
2022-11-29 14:26:36 +0100

    Update 'etc/libvirt/hooks/qemu.d/win10/prepare/begin/start.sh'

--- etc/libvirt/hooks/qemu.d/win10/prepare/begin/start.sh
+++ etc/libvirt/hooks/qemu.d/win10/prepare/begin/start.sh
@@ -5,26 +5,23 @@ set -x
 source "/etc/libvirt/hooks/kvm.conf"
 
 # Stop display manager
-systemctl stop lightdm.service
-
-# Stop pipewire
-pulse_pid=$(pgrep -u quentin pulseaudio)
-pipewire_pid=$(pgrep -u quentin pipewire-media)
-kill $pulse_pid
-kill $pipewire_pid
+systemctl stop sddm.service
 
 # Unbind VTconsoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
 # Unbind EFI Framebuffer
-# echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
 # Avoid a race condition by waiting a couple of seconds. This can be calibrated to be shorter or longer if required for your system
 sleep 5
 
 # Unload AMD kernel module
+modprobe -r drm_kms_helper
 modprobe -r amdgpu
+modprobe -r radeon
+modprobe -r drm
 
 # Detach GPU devices from host
 # Use your GPU and HDMI Audio PCI host device
@@ -34,4 +31,4 @@ virsh nodedev-detach $VIRSH_GPU_AUDIO
 # Load vfio module
 modprobe vfio
 modprobe vfio_pci
-modprobe vfio_iommu_type1
+modprobe vfio_iommu_type0
